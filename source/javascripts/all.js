6fa9844cd95bf528ad70124295a220663b9b8d0e
stephane delatorre <s.dlt@hotmail.fr>
2017-07-09 00:55:48 +0200

    done with navbar

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,8 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(".tab").on("click", function(e){
+    $(".tab").removeClass('active');
+    $(this).addClass('active');
+});
