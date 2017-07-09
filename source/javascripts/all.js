229ba7a65feaeab61fe93c749d7f30680199523f
stephane delatorre <s.dlt@hotmail.fr>
2017-07-09 16:56:55 +0200

    done with navbar

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,7 +2,38 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-$(".tab").on("click", function(e){
+$(document).on('ready', function() {
+  function scroll_to_anchor(anchor_id){
+    var tag = $(anchor_id);
+    $('body').animate({scrollTop: tag.offset().top - 70},'slow');
+  };
+
+  $(".tab").on("click", function(e){
     $(".tab").removeClass('active');
     $(this).addClass('active');
-});
+    e.preventDefault();
+    scroll_to_anchor($(this).attr("href"));
+  });
+
+  var topMenu = $(".navbar"),
+  topMenuHeight = topMenu.outerHeight() + 20,
+  menuItems = topMenu.find("a"),
+  scrollItems = menuItems.map(function(){
+    var item = $($(this).attr("href"));
+    if (item.length) { return item; }
+  });
+  console.log(topMenuHeight);
+
+  $(document).on('scroll',function(){
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+    var cur = scrollItems.map(function(){
+      if ($(this).offset().top <= fromTop)
+      return this;
+    });
+
+    cur = cur[cur.length-1];
+    var id = cur && cur.length ? cur[0].id : "";
+    menuItems.removeClass("active");
+    $('#' + id + '-tab').addClass("active");
+  })
+})
